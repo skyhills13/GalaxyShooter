dc58fdfe2e350c3add7e520574acb71a64aef0b1
박소은[eg102] <skyhills13@gmail.com>
2018-02-27 22:41:57 +0900

    make spawn manager

---
+++ Assets/Galaxy Shooter/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject enemy;
+    [SerializeField]
+    private GameObject[] powerUps;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(GenerateEnemy());
+		StartCoroutine(GeneratePowerUps());
+    }
+
+    IEnumerator GeneratePowerUps(){
+        while(true) {
+			int randomPowerUpId = Random.Range(0, powerUps.Length);
+			Instantiate(powerUps[randomPowerUpId], new Vector3(Random.Range(-7f, 7f), 7f, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    IEnumerator GenerateEnemy(){
+        while(true) {
+            Instantiate(enemy, new Vector3(Random.Range(-7f, 7f), 7f, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5f);
+        }
+    }
+}
